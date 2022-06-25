c02ad22ca68a0b758fa376ec774b51b4a7304e03
Andrey I. Esaulov <esaulov.andrey@gmail.com>
2022-06-25 17:57:25 +0200

    Initial Commit

---
+++ Scrumtimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumtimer
+//
+//  Created by Andrey Esaulov on 25.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumtimer/ScrumtimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumtimerApp.swift
+//  Scrumtimer
+//
+//  Created by Andrey Esaulov on 25.06.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumtimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
